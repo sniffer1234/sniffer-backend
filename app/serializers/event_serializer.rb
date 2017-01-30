88ed2f1d1934cbcf54afaaca2bdb36abfbd38a8c
Rafael Michels Motta <rafaelmotta021@gmail.com>
2017-01-29 23:46:08 -0200

    Added start_time

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,11 +1,16 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :starts_at, :ends_at, :no_time_to_end
+  attributes :id, :name, :description, :starts_at, :ends_at, :no_time_to_end, :start_time
+
+  def start_time
+    I18n.l(object.starts_at, format: :time)
+
+  end
 
   def starts_at
-    I18n.l(object.starts_at, format: :default)
+    I18n.l(object.starts_at, format: :abbreviated)
   end
 
   def ends_at
-    I18n.l(object.starts_at, format: :default)
+    I18n.l(object.starts_at, format: :abbreviated)
   end
 end
