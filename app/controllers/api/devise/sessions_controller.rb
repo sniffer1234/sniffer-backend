c3da5b5ad567cdfab0ddf897204a9a5cd706b873
Rafael Michels Motta <rafaelmotta021@gmail.com>
2017-02-03 10:21:57 -0200

    Sessions authentication

--- app/controllers/api/devise/sessions_controller.rb
+++ app/controllers/api/devise/sessions_controller.rb
@@ -2,9 +2,17 @@ class Api::Devise::SessionsController < Devise::SessionsController
   respond_to :json
 
   def create
+    puts '------ 1 ----'
+    puts '------ 1 ----'
+    
     self.resource = warden.authenticate!(auth_options)
+    puts '------ 2 ----'
+    puts '------ 2 ----'
 
     sign_in(resource_name, resource)
+    puts '------ 3 ----'
+    puts '------ 3 ----'
+
     render json: resource, root: 'data'
  end
 
