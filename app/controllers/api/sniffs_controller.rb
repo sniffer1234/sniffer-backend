1b8c31dd38fb0c1f17954d16f55ff7676ed3c1ed
Rafael Michels Motta <rafaelmotta021@gmail.com>
2017-02-17 14:04:50 -0200

    Database settings

--- app/controllers/api/sniffs_controller.rb
+++ app/controllers/api/sniffs_controller.rb
@@ -2,7 +2,7 @@ class Api::SniffsController < Api::BaseController
 
   # GET /api/establishment/:id/sniffs
   def index
-    @sniffs = Sniff.by_establishment(params[:establishment_id]).order(id: :desc)
+    @sniffs = Sniff.by_establishment(params[:establishment_id]).order(id: :desc).limit(10)
     render json: @sniffs, root: 'data'
   end
 end
