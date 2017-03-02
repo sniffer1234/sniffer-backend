b8ceb983f6ce6f9c97fe8364dae4f557535929db
Rafael Michels Motta <rafaelmotta021@gmail.com>
2017-03-02 14:38:24 -0300

    Adjust events serializers

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,7 +1,7 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :description,
-             :starts_at, :ends_at, :no_time_to_end,
-             :start_time, :cover, :when
+             :starts_at, :ends_at, :start_time,
+             :end_time, :cover, :when
 
   has_one :establishment, serializer: ShortEstablishmentSerializer
 
--- app/serializers/events_serializer.rb
+++ app/serializers/events_serializer.rb
@@ -1,10 +1,15 @@
-class EventsSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description,
-             :starts_at, :ends_at, :no_time_to_end,
-             :start_time, :establishment_name, :cover
+
+class EventSerializer < ActiveModel::Serializer
+  attributes :id, :name, :description
+             :starts_at, :ends_at, :start_time,
+             :end_time, :cover, :when, :establishment_name
 
   def start_time
     I18n.l(object.starts_at, format: :time)
+  end
+
+  def end_time
+    I18n.l(object.ends_at, format: :time)
   end
 
   def starts_at
@@ -12,7 +17,7 @@ class EventsSerializer < ActiveModel::Serializer
   end
 
   def ends_at
-    I18n.l(object.starts_at, format: :abbreviated)
+    I18n.l(object.ends_at, format: :abbreviated)
   end
 
 end
