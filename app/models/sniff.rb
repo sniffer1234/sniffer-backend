f2af31fdfec5236577bbe8b9f17e8705fd5e794d
Rafael Michels Motta <rafaelmotta021@gmail.com>
2017-03-08 11:05:37 -0300

    teste video

--- app/models/sniff.rb
+++ app/models/sniff.rb
@@ -54,20 +54,21 @@ class Sniff < ApplicationRecord
   end
 
   def set_video_duration
-    result = `ffmpeg -i #{self.video.staged_path} 2>&1`
-
-    puts "------ video stage ath ------"
-    puts "------ video stage ath ------"
-    puts self.video.staged_path
-    puts result
-    puts "------ video stage ath ------"
-    puts "------ video stage ath ------"
-
-
-    r = result.match("Duration: ([0-9]+):([0-9]+):([0-9]+).([0-9]+)")
-    duration = "#{ r[1].to_i*3600+r[2].to_i*60+r[3].to_i }.#{ r[1].to_i*3600+r[2].to_i*60+r[4].to_i }".to_f
-
-    self.duration = duration.round * 1000
+    self.duration = 3000
+    # result = `ffmpeg -i #{self.video.staged_path} 2>&1`
+    #
+    # puts "------ video stage ath ------"
+    # puts "------ video stage ath ------"
+    # puts self.video.staged_path
+    # puts result
+    # puts "------ video stage ath ------"
+    # puts "------ video stage ath ------"
+    #
+    #
+    # r = result.match("Duration: ([0-9]+):([0-9]+):([0-9]+).([0-9]+)")
+    # duration = "#{ r[1].to_i*3600+r[2].to_i*60+r[3].to_i }.#{ r[1].to_i*3600+r[2].to_i*60+r[4].to_i }".to_f
+    #
+    # self.duration = duration.round * 1000
 
   end
 
