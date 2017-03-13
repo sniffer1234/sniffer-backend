f7c34b7a1437455b9f129eb57cb26cf64ea3600c
Rafael Michels Motta <rafaelmotta021@gmail.com>
2017-03-13 17:36:23 -0300

    Rollback user

--- app/serializers/sniff_serializer.rb
+++ app/serializers/sniff_serializer.rb
@@ -1,7 +1,17 @@
 class SniffSerializer < ActiveModel::Serializer
-  attributes :id, :img, :video, :duration, :time_ago
+  attributes :id, :img, :video, :duration, :time_ago, :user
 
-  has_one :user, serializer: UserShortSerializer
+  def user
+    return {
+      id: object.user.id,
+      name: object.user.name,
+      first_name: object.user.first_name,
+      avatar: {
+        medium: object.user.avatar.url(:medium),
+        thumb: object.user.avatar.url(:thumb)
+      }
+    }
+  end
 
   def img
     unless object.img_file_size.nil?
